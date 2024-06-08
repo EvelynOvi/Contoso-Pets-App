b083d9f757e6527220046e6397e4f237d5837377
Evelyn Oviangbede <oviangbedeevelyn@gmail.com>
2024-06-08 20:55:30 +0100

    updated

--- Final/obj/Debug/net8.0/Final.AssemblyInfo.cs
+++ Final/obj/Debug/net8.0/Final.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Final")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+829980aef36e8d6377874605bc3dee2237c3d8f3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Final")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Final")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
